8d599d19fedcff4c8772d64e1c6f843f16548114
jeremy perry <jeremy.m.perry@gmail.com>
2019-08-02 18:00:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line (katz_deli)
     puts "The line is currently empty."
     
   else
-    current_line = "The line is currentl:"
+    current_line = "The line is currently:"
     katz_deli.each.with_index(1) do |person, i|
       current_line << " #{i}. #{peraon}"
     
