f9f56f227b88391cf4bbcb13dc1aface90f7e774
jeremy perry <jeremy.m.perry@gmail.com>
2019-08-02 18:19:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line (katz_deli)
     message = "The line is currently:"
     
     katz_deli.each_with_index do |value, index|
-        message += " #{index.to_i+1}. #{value}"
+        message += " #{index.to_i += 1}. #{value}"
         
       end
       
@@ -28,7 +28,14 @@ def take_a_number (katz_deli,name)
 
 end
 
-def now_serving ()
+def now_serving (katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served"
+  
+else
+    puts "Currently serving #{katz_deli.shift}."
+    
+  end
   
 
 end
